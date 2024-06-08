8b33079c004515e1d58db7d7cf9e52fd6db09aed
andrew.ryan.vanbeek@gmail.com <andrewvanbeek@andrews-Air-3.lan>
2024-06-08 07:50:49 -0400

    before swizzle

--- dev-docs-openapi.js
+++ dev-docs-openapi.js
@@ -323,13 +323,14 @@ const apiConfig = {
       // optional, instructs plugin to generate sidebar.js
       groupPathsBy: "tag", // group sidebar items by operation "tag"
     },
-    markdownGenerators: {
-      createApiPageMD: (metadata) => {
-         return importer.generateTempates("./tdocxcollection.yml", metadata, {postmanCollectionPath: "./tdocxpostman.json"})
-         // console.log("metadata", metadata);
+    // markdownGenerators: {
+    //   createApiPageMD: (metadata) => {
+    //      return importer.generateTempates("./tdocxcollection.yml", metadata, {postmanCollectionPath: "./tdocxpostman.json"})
+    //      // console.log("metadata", metadata);
          
-      },
-    },
+    //   },
+    
+    // },
   },
 };
 
